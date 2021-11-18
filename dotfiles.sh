67cc48577255b7c4c8342e7abfd240b5cf63f54c
John <git@stillconfusing.net>
2021-11-17 21:42:17 -0500

    added install script

---
+++ dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone --bare --recursive https://github.com/AreYouConfused/dotfiles.git $HOME/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+config pull
+config submodule update
+config config --local status.showUntrackedFiles no
