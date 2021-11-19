d827ae1599a994524651501800866aa3d9e81ec9
Still <50085876+AreYouConfused@users.noreply.github.com>
2021-11-19 12:32:44 -0500

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,6 @@
 git clone --bare --recursive https://github.com/AreYouConfused/dotfiles.git $HOME/.dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
-config pull
-config submodule update
-config config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME pull
+/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME submodule update
+/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
