c4ad2ace916a06d8939529d2530679f9bb144473
John <git@stillconfusing.net>
2021-11-23 11:27:51 -0500

    added README

--- dotfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone --bare --recursive https://github.com/AreYouConfused/dotfiles.git $HOME/.dotfiles
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-
-/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME pull
-/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME submodule update
-/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
