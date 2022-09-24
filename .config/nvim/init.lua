5ca26b0c2aaed8dec0d54068b9ee9186d6757932
still <git@stillconfusing.net>
2022-09-23 23:20:32 -0400

    removed vim files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,22 @@
 local cmd = vim.cmd
 local opt = vim.opt
 
-cmd('source ~/.config/nvim/vim/coc.vim')
+--cmd('source ~/.config/nvim/vim/coc.vim')
 cmd('source ~/.config/nvim/vim/keybinds.vim')
 cmd('source ~/.config/nvim/vim/auto.vim')
 require("plugins")
 require("plugin-config")
+require("coc-conf")
 
 cmd('set path+=**')
 cmd('colorscheme dracula')
 opt.termguicolors = true
 opt.number = true
-opt.tabstop = 8
-opt.sw = 8
-opt.sts = 8
+local spacing = 5
+opt.tabstop = spacing
+opt.sw = spacing
+opt.sts = spacing
 opt.cursorline = true
+
 cmd('set listchars=tab:-->,space:·,nbsp:␣,trail:•,eol:$,precedes:«,extends:»')
 
