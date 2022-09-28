cbf1e0c304d3b12690d7dcc71ecb3b22507f6b5c
still <git@stillconfusing.net>
2022-09-28 17:28:35 -0400

    disabled loading of netrw for use with nvim-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,11 @@
 local cmd = vim.cmd
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+require("keybinds")
 --cmd('source ~/.config/nvim/vim/coc.vim')
 --cmd('source ~/.config/nvim/vim/keybinds.vim')
 cmd('source ~/.config/nvim/vim/auto.vim')
 require("plugins")
-require("plugin-config")
-require("keybinds")
 require("options")
 --require("coc-conf")
 
