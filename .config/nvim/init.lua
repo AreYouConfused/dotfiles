a6e7d3b66af950bafc90ed5a0944a6debebfa0b6
still <git@stillconfusing.net>
2022-09-28 17:31:08 -0400

    removed .vim keybinds file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,10 +2,7 @@ local cmd = vim.cmd
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 require("keybinds")
---cmd('source ~/.config/nvim/vim/coc.vim')
---cmd('source ~/.config/nvim/vim/keybinds.vim')
 cmd('source ~/.config/nvim/vim/auto.vim')
 require("plugins")
 require("options")
---require("coc-conf")
 
