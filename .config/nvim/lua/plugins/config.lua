42fea369ae2853e906ef78f95a72375741b5befc
still <git@stillconfusing.net>
2022-09-29 22:07:34 -0400

    updated lsp attatch

--- .config/nvim/lua/plugins/config.lua
+++ .config/nvim/lua/plugins/config.lua
@@ -152,12 +152,14 @@ vim.g.coq_settings = { auto_start = 'shut-up' }
 local servers = { 'clangd', 'rust_analyzer', 'tsserver' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities({
-    -- on_attach = my_custom_on_attach,
+    on_attach = lsp_attach,
   }))
 end
 
 lspconfig['pyright'].setup{
-  require('coq').lsp_ensure_capabilities({}),
+  require('coq').lsp_ensure_capabilities({
+    on_attach = lsp_attach,
+  }),
   python = {
     analysis = {
       autoSearchPaths = true,
