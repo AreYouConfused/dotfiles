9fbe2356e0b8a8a04657c3e4f6d31c8d6ca5caae
still <git@stillconfusing.net>
2022-10-08 14:48:57 -0400

    switched from paq to packer

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,27 +1,2 @@
-require "paq" {
-    "savq/paq-nvim";                  -- Let Paq manage itself
-    "bfrg/vim-cpp-modern";
-    "joshdick/onedark.vim";
-    "junegunn/vim-easy-align";
-    --{"neoclide/coc.nvim", branch = "release"};
-    --"KabbAmine/vCoolor.vim";
-    --"IngoMeyer441/coc_current_word";
-    "voldikss/vim-floaterm";
-    "alvan/vim-closetag";
-    "tpope/vim-sleuth";
-    "lervag/vimtex";
-    "tmsvg/pear-tree";
-    "Mofiqul/dracula.nvim";
-    "nvim-treesitter/nvim-treesitter";
-    "lukas-reineke/indent-blankline.nvim";
-    "nvim-lualine/lualine.nvim";
-    "p00f/nvim-ts-rainbow";
-    "ms-jpq/coq_nvim";
-    "neovim/nvim-lspconfig";
-    "kyazdani42/nvim-web-devicons";
-    "kyazdani42/nvim-tree.lua";
-    "romgrk/barbar.nvim";
-    "yamatsum/nvim-cursorline";
-}
-
+require "plugins/packer"
 require "plugins/config"
