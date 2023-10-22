dd8ab34c96c391cabea0a04fd719bd993705927e
areyouconfused <git@stillconfusing.net>
2023-10-22 16:00:48 -0400

    added fish aliases

---
+++ .config/fish/functions/config.fish
@@ -0,0 +1,4 @@
+function config --wraps='/usr/bin/git --git-dir=/home/still/.dotfiles --work-tree=/home/still' --description 'alias config /usr/bin/git --git-dir=/home/still/.dotfiles --work-tree=/home/still'
+  /usr/bin/git --git-dir=/home/still/.dotfiles --work-tree=/home/still $argv
+        
+end
