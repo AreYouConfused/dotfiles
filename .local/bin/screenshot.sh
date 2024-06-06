48b96380ec469307f8c7ab638317db6f288ab8ff
Still <git@stillconfusing.net>
2024-06-06 19:50:41 -0400

    updated bindings and screenshot

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -4,23 +4,40 @@ set -eE
 SCREENSHOTDIR=$HOME/Pictures/Screenshots
 TIME=$(date +"%Y-%m-%dT%H:%M:%S")
 IMAGE_NAME="${SCREENSHOTDIR}/${TIME}.png"
-grim "$IMAGE_NAME"
+# grim "$IMAGE_NAME"
+#
+# # inotifywait -e close "$IMAGE_NAME"
+# feh "$IMAGE_NAME" &
+# FEH_PID=$!
+# trap cleanup SIGINT
+# trap cleanup ERR
+# cleanup(){
+# 	kill $FEH_PID
+# 	exit 2
+# }
+# echo $FEH_PID
+# #sleep 0.5
+# REGION=$(slurp -f '%wx%h+%x+%y')
+# kill $FEH_PID
+#
+# mogrify -crop "$REGION" "$IMAGE_NAME"
 
-# inotifywait -e close "$IMAGE_NAME"
-feh "$IMAGE_NAME" &
-FEH_PID=$!
+hyprpicker -rz &
+PICK_PID=$!
+cleanup(){
+	kill -9 $PICK_PID
+	exit 4
+}
 trap cleanup SIGINT
 trap cleanup ERR
-cleanup(){
-	kill $FEH_PID
-	exit 2
-}
-echo $FEH_PID
-#sleep 0.5
-REGION=$(slurp -f '%wx%h+%x+%y')
-kill $FEH_PID
 
-mogrify -crop "$REGION" "$IMAGE_NAME"
+hyprctl keyword animations:enabled false
+# sleep 0.2
+# REGION=$(slurp)
+# sleep 0.5
+grim -g "$(slurp)" "$IMAGE_NAME"
+hyprctl keyword animations:enabled true
+kill -9 $PICK_PID
 
 wl-copy < "$IMAGE_NAME"
 
